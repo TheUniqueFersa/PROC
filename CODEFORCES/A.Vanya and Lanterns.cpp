0d4823519c168732607480b8fe8ce788273c06df
TheUniqueFersa <holafersa@gmail.com>
2024-11-16 14:03:14 -0600

    fix(CODEFORCES): añade A. Vanya and Lanterns resuelto

--- CODEFORCES/A.Vanya and Lanterns.cpp
+++ CODEFORCES/A.Vanya and Lanterns.cpp
@@ -1,3 +1,4 @@
+//From Week 1 ADHOC
 #include <bits/stdc++.h>
 #define fastIo() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define ll long long
@@ -12,23 +13,31 @@ typedef vector<vi> vii;
 typedef vector<vpi> vvpi;
 
 void solve() {
-    int n, l;
+    int n;
+    ll l;
     cin >> n >> l;
-    vi a(n);
-    for(int i = 0; i < n; i++) cin >> a[i];
-    sort(a.begin(), a.end());
-    double maxDist = 0;
-    for(int i = 0; i < n - 1; i++){
-        maxDist = max(maxDist, double(a[i + 1] - a[i]));
+    vector<ll> a(n,0);
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
     }
-    maxDist = max(maxDist, (double)a[0]);
-    maxDist = max(maxDist, (double)l - a[n - 1]);
-    cout << fixed << setprecision(10) << maxDist << endl;
+    double maxi = 0;
+    sort(a.begin(), a.end());
+    for(int i = 0; i < n-1; i++){
+        maxi = max(maxi, double(a[i+1] - a[i]));
+    }
+    double bi = 0, bd = 0;
+    maxi/=2.0;
+    if(a[0] != 0){
+        bi = a[0];
+    }
+    if(a[n-1] != l){
+        bd = l-a[n-1];
+    }
+    maxi = max(maxi, max(bi, bd));
+    cout << fixed << setprecision(10) << maxi << '\n';
 }
 
 int main() {
     fastIo();
-    int t = 1;
-    cin >> t;
-    for(int i = 0; i < t; i++) solve();
+    solve();
 }
