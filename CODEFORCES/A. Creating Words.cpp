785ce38e44ab1e518bcf88285c96793bd43a0eec
TheUniqueFersa <holafersa@gmail.com>
2024-11-28 00:00:04 -0600

    recovering codeforces streak

---
+++ CODEFORCES/A. Creating Words.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define fastIo() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+using namespace std;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<pi> vpi;
+typedef vector<vi> vii;
+typedef vector<vpi> vvpi;
+
+void solve() {
+    string s1, s2;
+    cin >> s1 >> s2;
+    char c;
+    c = s1[0];
+    s1[0] = s2[0];
+    s2[0] = c;
+    cout << s1 << " " << s2 << '\n';
+}
+
+int main() {
+    fastIo();
+    int t = 1;
+    cin >> t;
+    for(int i = 0; i < t; i++) solve();
+}
---
+++ CODEFORCES/A. Magnets.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define fastIo() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+using namespace std;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<pi> vpi;
+typedef vector<vi> vii;
+typedef vector<vpi> vvpi;
+
+void solve() {
+    int n, groups = 0;
+    string previous, s;
+    cin >> n;
+    cin >> previous;
+    while(n--){
+        cin >> s;
+        if(s != previous){
+            groups++;
+            previous = s;
+        }
+    }
+    cout << groups << '\n';
+}
+
+int main() {
+    fastIo();
+    solve();
+}
