6bf3d6a03c40d40ddaa0115e70087d01c6f0be2d
TheuniqueFersa <holafersa@gmail.com>
2024-12-16 21:41:30 -0600

    feat(CODEFORCES): añade problema D del Div4 del 15-12-2024

--- CODEFORCES/Divs/Div 4 2024/D. Harder Problem.cpp
+++ CODEFORCES/Divs/Div 4 2024/D. Harder Problem.cpp
@@ -1,1 +1,64 @@
 //E. Insane problem - not solved
+#include <bits/stdc++.h>
+#define fastIO() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> ii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vii> vvii;
+#define F first
+#define S second
+#define PB push_back
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define sz(a) (int)(a.size())
+#define fori(i,a,n) for(int i = a; i < n; i++)
+#define in(v) for(auto &x : v) cin >> x;
+#define out(v) for(auto x : v) cout << x << " "; cout<<endl;
+#define endl '\n'
+const int MOD = 1e9+7;
+const int INF = INT_MAX;
+const long long LLINF = LLONG_MAX;
+const double EPS = 1e-9;
+/*
+void setIO() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+*/
+void solve() {
+    int n;
+    cin>>n;
+    vi a(n), b;
+    in(a);
+    set<int> s, t;
+    fori(i, 1, n+1) t.insert(i);
+    for(auto x:a) s.insert(x);
+    for(auto &each:a){
+        if(s.count(each)){
+            s.erase(each);
+            t.erase(each);
+            b.PB(each);
+        }
+    }
+    for(auto &each:t){
+        b.PB(each);
+    }
+    out(b);
+}
+
+int main() {
+    fastIO();
+//    setIO();
+    int t;
+    cin >> t;
+    while( t-- ) solve();
+    return 0;
+}
