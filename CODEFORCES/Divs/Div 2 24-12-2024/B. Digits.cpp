0e58f02ddda1c60d35b138123a7aa2fd27c5e0f9
TheuniqueFersa <holafersa@gmail.com>
2024-12-24 15:27:10 -0600

    feat(CODEFORCES - Div 2): añade problemas del Div2 24-12-2024

---
+++ CODEFORCES/Divs/Div 2 24-12-2024/B. Digits.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#define fastIO() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> ii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vii> vvii;
+#define F first
+#define S second
+#define PB push_back
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define sz(a) (int)(a.size())
+#define fori(i,a,n) for(int i = a; i < n; i++)
+#define in(v) for(auto &x : v) cin >> x;
+#define endl '\n'
+#define out(v) for(auto x : v) cout << x << " "; cout<<endl;
+const int MOD = 1e9+7;
+const int INF = INT_MAX;
+const long long LLINF = LLONG_MAX;
+const double EPS = 1e-9;
+/*
+void setIO() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+*/
+void solve() {
+    ll n, d;
+    cin >> n >> d;
+    cout << "1 ";
+    //3
+    if(d % 3 == 0 || n>=3) cout << "3 ";
+    //5
+    if(d % 5 == 0) cout << "5 ";
+    //7
+    if(d % 7 == 0 || n>=3) cout << "7 ";
+    //9
+    if(d % 9 == 0||n>=6||(d%3==0 && n>=3)) cout << "9 ";
+    cout << endl;
+}
+int main() {
+    fastIO();
+//    setIO();
+    int t;
+    cin >> t;
+    while( t-- ) solve();
+    return 0;
+}
