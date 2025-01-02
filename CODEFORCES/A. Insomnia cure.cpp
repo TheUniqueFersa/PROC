93aacc9a7fe3cc428e33742bea9f00a9f6e0cd72
TheUniqueFersa <holafersa@gmail.com>
2025-01-01 22:42:54 -0600

    feat(CODEFORCES): añade problema A. Insomnia cure.cpp

---
+++ CODEFORCES/A. Insomnia cure.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#define fastIO() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> ii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vii> vvii;
+#define F first
+#define S second
+#define PB push_back
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define sz(a) (int)(a.size())
+#define fori(i,a,n) for(int i = a; i < n; i++)
+#define in(v) for(auto &x : v) cin >> x;
+#define endl '\n'
+#define out(v) for(auto x : v) cout << x << " "; cout<<endl;
+const int MOD = 1e9+7;
+const int INF = INT_MAX;
+const long long LLINF = LLONG_MAX;
+const double EPS = 1e-9;
+/*
+void setIO() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+*/
+void solve() {
+    int k, l, m, n, d;
+    cin >> k >> l >> m >> n >> d;
+    int ans = 0;
+    
+    fori(i, 1, d+1) {
+        if(i % k == 0 || i % l == 0 || i % m == 0 || i % n == 0) ans++;
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    fastIO();
+//    setIO();
+    solve();
+    return 0;
+}
