2df0c4f05e35b5013c5a36d8dc624d1a97207553
TheUniqueFersa <holafersa@gmail.com>
2025-01-06 21:27:27 -0600

    feat(CODEFORCES): añade problema A. Games

---
+++ CODEFORCES/A. Games.cpp
@@ -0,0 +1,62 @@
+/*
+    A. Games
+ - theuniqueFersa: YI!!
+    aprox 4 min | 06/01/2025
+*/
+#include <bits/stdc++.h>
+#define fastIO() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> ii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vii> vvii;
+#define F first
+#define S second
+#define PB push_back
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define sz(a) (int)(a.size())
+#define fori(i,a,n) for(int i = a; i < n; i++)
+#define in(v) for(auto &x : v) cin >> x;
+#define endl '\n'
+#define out(v) for(auto x : v) cout << x << " "; cout<<endl;
+const int MOD = 1e9+7;
+const int INF = INT_MAX;
+const long long LLINF = LLONG_MAX;
+const double EPS = 1e-9;
+/*
+void setIO() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+*/
+void solve() {
+    int n;
+    cin >> n;
+    vii a(n); 
+    fori(i, 0, n){
+        cin >> a[i].F >> a[i].S;
+    }
+    int res = 0;
+    fori(i, 0, n){
+        fori(j, 0, n){
+            if(i != j) 
+                if(a[i].F == a[j].S) res ++;
+        }
+    }
+    cout << res << endl;
+}
+
+int main() {
+    fastIO();
+//    setIO();
+    solve();
+    return 0;
+}
