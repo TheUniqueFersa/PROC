ce96cfd5bb751a6df2bd37c8d9b23a1b9826961a
TheUniqueFersa <holafersa@gmail.com>
2025-01-11 22:56:39 -0600

    feat(CODEFORCES): añade problema A. Soft Drinking

---
+++ CODEFORCES/A. Soft Drinking.cpp
@@ -0,0 +1,51 @@
+/*
+    A. Soft Drinking
+ - theuniqueFersa: YI!!
+    00:21:45 | 11/01/2025
+*/
+#include <bits/stdc++.h>
+#define fastIO() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> ii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vii> vvii;
+#define F first
+#define S second
+#define PB push_back
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define sz(a) (int)(a.size())
+#define fori(i,a,n) for(int i = a; i < n; i++)
+#define in(v) for(auto &x : v) cin >> x;
+#define endl '\n'
+#define out(v) for(auto x : v) cout << x << " "; cout<<endl;
+const int MOD = 1e9+7;
+const int INF = INT_MAX;
+const long long LLINF = LLONG_MAX;
+const double EPS = 1e-9;
+/*
+void setIO() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+*/
+void solve() {
+    int n, k, l, c, d, p, nl, np;
+    cin>>n>>k>>l>>c>>d>>p>>nl>>np;
+    cout << (min(min(k*l / nl, c*d), p/np))/n;
+}
+
+int main() {
+    fastIO();
+//    setIO();
+    solve();
+    return 0;
+}
