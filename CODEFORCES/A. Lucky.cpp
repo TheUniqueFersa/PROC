4c37938c84193361fa67d655c1f7505a886254be
TheUniqueFersa <holafersa@gmail.com>
2025-01-20 23:40:26 -0600

    feat(CODEFORCES): añade A. Lucky?

---
+++ CODEFORCES/A. Lucky.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#define fastIO() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> ii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vii> vvii;
+#define F first
+#define S second
+#define PB push_back
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define sz(a) (int)(a.size())
+#define fori(i,a,n) for(int i = a; i < n; i++)
+#define in(v) for(auto &x : v) cin >> x;
+#define endl '\n'
+#define out(v) for(auto x : v) cout << x << " "; cout<<endl;
+const int MOD = 1e9+7;
+const int INF = INT_MAX;
+const long long LLINF = LLONG_MAX;
+const double EPS = 1e-9;
+/*
+void setIO() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+*/
+int t(char s){
+    return s-'0';
+}
+void solve() {
+    string s;
+    cin >> s;
+    if(t(s[0]) + t(s[1]) + t(s[2]) == t(s[3]) + t(s[4]) + t(s[5])) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
+
+int main() {
+    fastIO();
+//    setIO();
+    int t;
+    cin >> t;
+    while( t-- ) solve();
+    return 0;
+}
