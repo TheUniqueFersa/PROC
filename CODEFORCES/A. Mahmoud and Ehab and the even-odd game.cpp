0daa266f8c858e95a1b9542940cdd67af656e133
theuniquefersa <holafersa@gmail.com>
2025-03-16 01:00:37 -0600

    feat(CF): adds solution to 959A

---
+++ CODEFORCES/A. Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,16 @@
+/*
+    https://codeforces.com/problemset/problem/959/A
+ - theuniqueFersa: YI!!
+    ? | ?
+*/
+#include <iostream>
+
+using namespace std;
+#define endl '\n'
+int main(){
+    int n;
+    cin >> n;
+    if(n%2==0)cout << "Mahmoud" << endl;
+    else cout <<"Ehab" << endl;
+    return 0;
+}
