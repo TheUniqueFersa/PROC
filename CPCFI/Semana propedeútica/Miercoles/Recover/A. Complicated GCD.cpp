8931a51e0057b80839adc3a136cb5893d88bf71e
theuniquefersa <holafersa@gmail.com>
2025-04-27 00:32:31 -0600

    feat(6G - Mi): adds solution to A

---
+++ CPCFI/Semana propedeútica/Miercoles/Recover/A. Complicated GCD.cpp
@@ -0,0 +1,19 @@
+/*
+    6G - Mi.A
+    theuniqueFersa: YI!!
+    ? | 27/04/2025
+*/
+#include <iostream>
+#define endl '\n'
+using namespace std;
+
+void solve(){
+    string s, s2;
+    cin>>s>>s2;
+    if(s == s2) cout << s << endl;
+    else cout << 1 << endl;
+}
+int main(){
+    solve();
+    return 0;
+}
