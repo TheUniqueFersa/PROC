af0c3efac5fc7c54bb1791ab4ce441eaac43c2fd
theuniquefersa <holafersa@gmail.com>
2025-04-28 16:38:57 -0600

    feat(USACO): adds template to USACO problems

---
+++ USACO/plantillaUSACO.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <fstream>
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)v.size()
+#define fori(a,b,c) for(int a = b; a<c; a++)
+#define PB push_back
+using namespace std;
+
+void solve(){
+
+    ifstream read("lineup.in");
+    //use read instead of cin
+
+    ofstream out("lineup.out");
+    //use out instead of cout
+
+
+}
+int main(){
+    solve();
+    return 0;
+}
