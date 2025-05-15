5921487cedf37c41e95bf0a2ddd650aa7559ed7b
theuniquefersa <holafersa@gmail.com>
2025-05-14 22:36:23 -0600

    refactor(Kruskal MST): fixes initial implementation

--- PROC/Graphs/MST/Kruskal.cpp
+++ PROC/Graphs/MST/Kruskal.cpp
@@ -1,5 +1,43 @@
 /*
-Minimum Spanning Tree - Kruskal with Disjoint Set Union
-from: https://cp-algorithms.com/graph/mst_kruskal_with_dsu.html 'El Einstein'
-
+Minimum Spanning Tree - Kruskal simplest implementation
+from: https://cp-algorithms.com/graph/mst_kruskal.html 'El Einstein'
 */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define endl '\n'
+using namespace std;
+
+struct Edge {
+    int u, v, weight;
+    bool operator<(Edge const& other) {
+        return weight < other.weight;
+    }
+};
+
+int main(){
+    int n;
+    vector<Edge> edges;
+
+    int cost = 0;
+    vector<int> tree_id(n);
+    vector<Edge> result;
+    for (int i = 0; i < n; i++)
+        tree_id[i] = i;
+
+    sort(edges.begin(), edges.end());
+
+    for (Edge e : edges) {
+        if (tree_id[e.u] != tree_id[e.v]) {
+            cost += e.weight;
+            result.push_back(e);
+
+            int old_id = tree_id[e.u], new_id = tree_id[e.v];
+            for (int i = 0; i < n; i++) {
+                if (tree_id[i] == old_id)
+                    tree_id[i] = new_id;
+            }
+        }
+    }
+    return 0;
+}
